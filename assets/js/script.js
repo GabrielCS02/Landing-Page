b874de551a98a65c6eaac349d30b7cb6069b5f84
Gabriel Costa <gacosil0204@gmail.com>
2022-01-14 12:58:43 -0300

    Adicionando validation para os campos do contato

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,7 @@ let mapa = document.querySelector("#mapa")
 nome.style.width = "100%"
 email.style.width= "100%"
 
+
 function validaNome(){
     let txtNome = document.querySelector("#txtNome")
     if(nome.value.length<3){
